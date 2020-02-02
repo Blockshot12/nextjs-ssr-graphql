ba9ef8db0d6d43c96d221a37238aa0a4f9b35023
François van Leersum <f.vanleersum@labdigital.nl>
2020-02-02 15:48:44 +0100

    Add components + public folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,32 @@
-import withApollo from "../lib/with-apollo";
-import Link from "next/link";
-import { useViewerQuery } from "../lib/viewer.graphql";
+import Link from 'next/link'
+import Head from 'next/head'
+
+import Nav from '../components/Nav'
+import withApollo from '../lib/with-apollo'
+import { useViewerQuery } from '../lib/viewer.graphql'
 
 const Index = () => {
-  const { data } = useViewerQuery();
+  const { data } = useViewerQuery()
 
   if (data) {
-    const { viewer } = data;
+    const { viewer } = data
     return (
       <div>
-        You're signed in as {viewer.name} and you're {viewer.status} goto{" "}
+        <Head>
+          <title>Home</title>
+          <link rel="" href="/favicon.ico" />
+        </Head>
+        <Nav />
+        You're signed in as {viewer.name} and you're {viewer.status} goto{' '}
         <Link href="/about">
           <a>static</a>
-        </Link>{" "}
+        </Link>{' '}
         test page.
       </div>
-    );
+    )
   }
 
-  return <div>...</div>;
-};
+  return <div>...</div>
+}
 
-export default withApollo(Index);
+export default withApollo(Index)
